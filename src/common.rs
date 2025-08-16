abd2243f01655111befe1e7d97a798760d513c09
SixXue <six.xue@outlook.com>
2025-08-16 09:18:17 +0800

    wlxan1_rustdesk_api UPDATE

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://116.131.41.227:21114".to_owned()
+    "https://wlxan1_rustdesk_api.usersdream.com".to_owned()
 }
 
 #[inline]

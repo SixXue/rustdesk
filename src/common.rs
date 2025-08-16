f734e7a6d7d82cee89b35b0a90bb5e38224e3dff
SixXue <six.xue@outlook.com>
2025-08-16 13:34:10 +0800

    修改替换usersdream-API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://wlxan1_rustdesk_api.usersdream.com".to_owned()
+    "https://rustdesk-api.usersdream.com".to_owned()
 }
 
 #[inline]
